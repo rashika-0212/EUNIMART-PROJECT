baed637ca151ae634a0f1ebfdcb93948713beb28
RASHIKA K V <107302130+rashika-0212@users.noreply.github.com>
2023-02-24 11:48:19 +0530

    Add files via upload

---
+++ storage.js
@@ -0,0 +1,19 @@
+
+function login(){
+    
+    alert("saved");
+
+    sessionStorage.setItem("Name",inputBoxes[0].value)
+    sessionStorage.setItem("pno",inputBoxes[1].value)
+    sessionStorage.setItem("Email",inputBoxes[2].value)
+
+    redirect("home.html");
+    //window.location.replace("/home.html");
+}
+
+
+function logout(){
+
+    sessionStorage.clear();
+    redirect("login.html")
+}
